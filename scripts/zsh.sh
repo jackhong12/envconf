b38df777ddb0adb74ab9014fd2298bbce68f7117
jack <jackhuang1205@gmail.com>
2023-01-25 14:43:41 +0000

    Show options of zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
+
+# options:
+help_msg="
+options:
+    -h, --help: show help messages
+    -p10k     : choose p10k as theme
+    -no-check : don't update oh-my-zsh
+"
+
+
+# theme
+theme=''
+no_check=false
+is_help=false
+for i in $*; do
+    # theme
+    if [ $i == "-p10k" ]
+    then
+        theme='p10k'
+    fi
+
+    # avoid updating oh-my-zsh
+    if [ $i == "-no-check" ]
+    then
+        no_check=true
+    fi
+
+    # help option
+    if [ $i == "-h" ] || [ $i == "--help" ]
+    then
+        is_help=true
+    fi
+done
+
+# help option
+if [ $is_help == true ]
+then
+    echo "${help_msg}"
+    exit 0
+fi
+
+# install dependencies
 sudo apt-get install git zsh wget curl -y
 sudo apt-get install net-tools -y
 sudo apt-get install powerline -y
-
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /tmp/install.sh
 sh /tmp/install.sh
 
@@ -16,27 +57,16 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 rm -f ${HOME}/.zshrc
 
 # theme
-is_theme=false
-no_check=false
-for i in $*; do
-    if [ $i == "-p10k" ]
-    then
-        # powerlevel10k
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-        printf "ZSH_THEME=\"powerlevel10k/powerlevel10k\"\n\n" >> ${HOME}/.zshrc
-        printf "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh\n\n" >> ${HOME}/.zshrc
-        is_theme=true
-        cp ./config/zsh/.p10k.zsh ${HOME}
-    fi
-    # avoid updating oh-my-zsh
-    if [ $i == "-no-check" ]
-    then
-        no_check=true
-    fi
-done
-
-if [ $is_theme != true ]
+# powerlevel10k
+if [ $theme == 'p10k' ]
 then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    printf "ZSH_THEME=\"powerlevel10k/powerlevel10k\"\n\n" >> ${HOME}/.zshrc
+    printf "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh\n\n" >> ${HOME}/.zshrc
+    is_theme=true
+    cp ./config/zsh/.p10k.zsh ${HOME}
+# rubby
+else
     printf "\nZSH_THEME=\"robbyrussell\"\n\n" >> ${HOME}/.zshrc
 fi
 
